2888643777861f561035004164a0595c5810dacd
Halazv2 <halaziani2003@gmail.com>
2023-01-05 16:14:19 +0100

    schema for Account

---
+++ server/src/models/Acounts.model.ts
@@ -0,0 +1,27 @@
+import {Model, Schema, model, Document} from "mongoose";
+
+export interface IAccount extends Document {
+  name: string;
+  email: string;
+  pin: string;
+  balance: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+interface IAccountModel extends Model<IAccount> {}
+
+
+const schema = new Schema<IAccount>(
+  {
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true, lowercase: true},
+    pin: {type: String, required: true},
+    balance: {type: Number, required: true},
+  },
+  {timestamps: true}
+);
+
+const Account = model<IAccount, IAccountModel>("Account", schema);
+
+export default Account;
