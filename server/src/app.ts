44767df1c57785be4e8b63a9d5ea5a787b7afc23
Halazv2 <halaziani2003@gmail.com>
2023-01-05 15:57:56 +0100

    logResponseTime & middleware that handles errors & Parsing the body & parses the body of the request & compresses the response body

---
+++ server/src/app.ts
@@ -0,0 +1,44 @@
+import bodyParser from "body-parser";
+import compression from "compression";
+import path from "path";
+import express, {Request, Response, NextFunction} from "express";
+import router from "./routes";
+const app = express();
+
+function logResponseTime(req: Request, res: Response, next: NextFunction) {
+  const startHrTime = process.hrtime();
+
+  res.on("finish", () => {
+    const elapsedHrTime = process.hrtime(startHrTime);
+    const elapsedTimeInMs = elapsedHrTime[0] * 1000 + elapsedHrTime[1] / 1e6;
+    const message = `${req.method} ${res.statusCode} ${elapsedTimeInMs}ms\t${req.path}`;
+
+    console.log(message);
+  });
+
+  next();
+}
+
+app.use(logResponseTime);
+
+
+/* A middleware that compresses the response body. */
+app.use(compression());
+/* A middleware that parses the body of the request and makes it available in the req.body property. */
+app.use(bodyParser.json());
+/* Parsing the body of the request and making it available in the req.body property. */
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(router);
+
+app.use((err: {status: any; message: any}, req: Request, res: Response, next: NextFunction) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+
+  return res.status(err.status || 500).json({
+    error: err.message,
+  });
+});
+
+export default app;
