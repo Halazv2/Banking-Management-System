fbefcfd648b8b2c37d42227ff786554f1051f941
Halazv2 <halaziani2003@gmail.com>
2023-01-05 16:06:23 +0100

    logger for development

---
+++ server/src/logger.ts
@@ -0,0 +1,34 @@
+import {createLogger, format, transports} from "winston";
+import ConsoleLoggerTransport from "./config/winston-console-transport";
+
+const logTransports = [
+  new transports.File({
+    level: "error",
+    filename: "./logs/error.log",
+    format: format.json({
+      replacer: (key, value) => {
+        if (key === "error") {
+          return {
+            message: (value as Error).message,
+            stack: (value as Error).stack,
+          };
+        }
+        return value;
+      },
+    }),
+  }),
+  new ConsoleLoggerTransport(),
+];
+
+/** is used to log messages in the application
+  * it is used in the following way:
+  * logger.log({level: "info", message: "message to log", error: err});
+ . */
+const logger = createLogger({
+  format: format.combine(format.timestamp()),
+  transports: logTransports,
+  defaultMeta: {service: "api"},
+  level: process.env.NODE_ENV === "development" ? "silly" : "info",
+});
+
+export default logger;
