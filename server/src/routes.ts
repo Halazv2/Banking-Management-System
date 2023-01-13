cb3d613402b56d2c5eeb706e3486374b7ef6edd4
halaziani <h.ziani@mysolutech.com>
2023-01-13 16:40:57 +0100

    add auth and test verify Token

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -3,6 +3,7 @@ import swaggerUi from "swagger-ui-express";
 const swaggerDocument = require("../openapi.json");
 import * as AccountsController from "./controllers/";
 import verifySignUp from "./middlewares/verifySignUp";
+import authJwt from "./middlewares/authJWT";
 
 const swaggerUiOptions = {
   customCss: ".swagger-ui .topbar { display: none }",
@@ -10,11 +11,12 @@ const swaggerUiOptions = {
 };
 
 const router = Router();
-router.get("/account", AccountsController.Accounts);
+router.get("/account", [authJwt.verifyToken], AccountsController.Accounts);
 router.post("/account", [verifySignUp.checkDuplicateEmail], AccountsController.AddAccount);
 router.get("/account/:id", AccountsController.GetAccountsByID);
 router.delete("/account/:id", AccountsController.DeleteAccount);
 router.get("/account/search/:name&:email&:pin", AccountsController.SearchAccount);
+router.post("/auth", AccountsController.Auth);
 
 if (process.env.NODE_ENV === "development") {
   router.use("/dev/api-docs", swaggerUi.serve);
