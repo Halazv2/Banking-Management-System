805ef83a938cb6580620289645db6d09110d8bbb
halaziani <h.ziani@mysolutech.com>
2023-01-13 17:31:28 +0100

    create Routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,22 @@
+import {Navigate, useRoutes} from "react-router-dom";
+// pages
 import Landing from "./pages/Landing";
 import TopNavbar from "../src/components/Nav/TopNavbar";
+import NotFound from "./pages/error/404";
+import UnAuthorized from "./pages/error/401";
 
 function App() {
+  const routes = useRoutes([
+    {path: "/", element: <Landing />},
+    {path: "/unauthorized", element: <UnAuthorized />},
+    {path: "/404", element: <NotFound />},
+    {path: "*", element: <Navigate to='/404' />},
+  ]);
 
   return (
     <>
       <TopNavbar />
-      <Landing />
+      {routes}
     </>
   );
 }
